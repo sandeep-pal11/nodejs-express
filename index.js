447b8ae7205a50b0dca344baa98ee722caaca1a5
sandip <sandeeppal8471@gmail.com>
2026-04-20 11:30:03 +0530

    install mongosee

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from 'express';
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));  
+
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');  
@@ -10,6 +13,18 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about',{title: 'About Page', description: 'This is the about page.'});  
 });
+
+app.get('/form', (req, res) => {
+    res.render('form');  
+}); 
+
+
+
+
+
+
+
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
